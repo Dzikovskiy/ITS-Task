35b2232bffe4903cb59766d0df151dab3a84e5e9
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2021-03-10 01:05:52 +0300

    cors allowed methods fix
    cors allowed methods fix

--- src/main/java/dzikovskiy/config/WebConfig.java
+++ src/main/java/dzikovskiy/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("*");
+        registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("GET", "PUT", "POST");
     }
 }
