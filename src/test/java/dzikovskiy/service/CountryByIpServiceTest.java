e0da52694203241e4b06cf58d059a2acdd315339
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2021-03-09 23:17:47 +0300

    tests for CountryByIpService
    tests for CountryByIpService

--- src/test/java/dzikovskiy/service/CountryByIpServiceTest.java
+++ src/test/java/dzikovskiy/service/CountryByIpServiceTest.java
@@ -1,27 +1,58 @@
 package dzikovskiy.service;
 
+import com.maxmind.geoip2.exception.AddressNotFoundException;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+@RunWith(SpringRunner.class)
 class CountryByIpServiceTest {
 
-    @Test
-    void getCountryIsoCode() throws IOException, GeoIp2Exception {
-        CountryByIpService countryByIpService = new CountryByIpService();
-        String ip = "77.111.246.6";
-        String ip2 = "77.111.246.6";
-        String ip3 = "46.216.38.234";
-        String countryCode = "US";
-        String countryCode2 = "ZZ";
-        String countryCode3 = "BY";
+    private CountryByIpService countryByIpService;
 
-        assertEquals(countryCode,countryByIpService.getCountryIsoCode(ip));
-        assertNotEquals(countryCode2,countryByIpService.getCountryIsoCode(ip2));
-        assertEquals(countryCode3,countryByIpService.getCountryIsoCode(ip3));
+    @BeforeEach
+    public void setUp() throws IOException {
+        countryByIpService = new CountryByIpService();
 
     }
+
+    @ParameterizedTest
+    @CsvSource({"77.111.246.6,US", "46.216.38.234,BY"})
+    void getCountryIsoCode_ShouldGenerateTheExpectedCountryCodePositiveTest(String ip, String countryCode) throws IOException, GeoIp2Exception {
+        assertEquals(countryCode, countryByIpService.getCountryIsoCode(ip));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"77.111.246.6,BY", "77.111.246.6,ZZ", "46.216.38.234,US"})
+    void getCountryIsoCode_ShouldGenerateTheExpectedCountryCodeNegativeTest(String ip, String countryCode) throws IOException, GeoIp2Exception {
+        assertNotEquals(countryCode, countryByIpService.getCountryIsoCode(ip));
+    }
+    
+
+    @ParameterizedTest
+    @ValueSource(strings = {"127.0.0.1"})
+    void getCountryIsoCode_ShouldThrowGeoIp2ExceptionForIpThatNotInDb(String ip) {
+        Assertions.assertThrows(GeoIp2Exception.class, () -> {
+            countryByIpService.getCountryIsoCode(ip);
+        });
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void getCountryIsoCode_ShouldThrowExceptionForNullAndEmptyInputs(String ip) {
+        Assertions.assertThrows(AddressNotFoundException.class, () -> {
+            countryByIpService.getCountryIsoCode(ip);
+        });
+    }
+
+
 }
